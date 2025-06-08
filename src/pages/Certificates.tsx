e8a68f20032b020d6c8c7ed5a53a7df85a725ece
Pentapalli Uday Kumar <udaykumar102004@gmail.com>
2025-06-08 15:31:59 +0530

    Update Certificates.tsx

--- src/pages/Certificates.tsx
+++ src/pages/Certificates.tsx
@@ -27,7 +27,7 @@ const Certificates = () => {
       description: "Covers essential employability skills including communication, soft skills, and digital literacy for young professionals.",
       issueDate: "April 2025",
       category: "Professional Development",
-      link: "#"
+      link: "https://drive.google.com/drive/folders/1uEa_z2__ysv96d-duDaHrUShStPy6Oop"
     },
     {
       title: "Software Engineering and Agile Software Development",
